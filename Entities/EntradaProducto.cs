5df5a7088b77b888a57032f2f5365348dc27ff85
Gianluis Ivan Gonzalez Caraballo <gianluis_gonzalez@ucne.edu.do>
2018-10-31 14:33:53 -0400

    Creando las entidades.

---
+++ Entities/EntradaProducto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class EntradaProducto
+    {
+        [Key]
+        public int EntradaId { get; set; }
+        public string Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public int ProductoId { get; set; }
+
+        public EntradaProducto()
+        {
+            EntradaId = 0;
+            Fecha = string.Empty;
+            Cantidad = 0;
+            ProductoId = 0;
+        }
+
+        public EntradaProducto(int entradaId, string fecha, int cantidad, int productoId)
+        {
+            EntradaId = entradaId;
+            Fecha = fecha;
+            Cantidad = cantidad;
+            ProductoId = productoId;
+        }
+    }
+}
---
+++ Entities/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Ordenes { get; set; }
+
+        public Usuario()
+        {
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            Email = string.Empty;
+            Password = string.Empty;
+            Ordenes = 0;
+        }
+
+        public Usuario(int usuarioId, string nombre, string apellido, string email, string password, int ordenes)
+        {
+            UsuarioId = usuarioId;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Password = password;
+            Ordenes = ordenes;
+        }
+    }
+}
