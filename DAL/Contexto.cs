9cf1815ca7385567126ffd53fc65fe3d7820f0ae
Gianluis Ivan Gonzalez Caraballo <gianluis_gonzalez@ucne.edu.do>
2018-10-31 14:40:01 -0400

    Creando el contexto en DAL.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<EntradaProducto> Entradas { get; set; }
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenesDetalles { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
