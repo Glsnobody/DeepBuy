225d31ec8fc8df7ba165988e9ee5c8d18a2ad761
Gianluis Ivan Gonzalez Caraballo <gianluis_gonzalez@ucne.edu.do>
2018-11-18 18:59:07 -0400

    Testeando los repositorios (Passed).

---
+++ DeepBuy.Tests/RepositorioEntradaTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeepBuy.Tests
+{
+    [TestClass]
+    public class RepositorioEntradaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
